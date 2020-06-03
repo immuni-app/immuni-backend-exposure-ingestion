b6fb584a1b56f3a5ba487a594d1cb1a86ddfcdd9
Omar Cardia <oc@bendingspoons.com>
2020-06-03 15:32:13 +0200

    chore(monitoring): order metrics alphabetically and small documentation fixes

--- immuni_exposure_ingestion/monitoring/celery.py
+++ immuni_exposure_ingestion/monitoring/celery.py
@@ -2,37 +2,37 @@ from prometheus_client import Counter, Gauge
 
 from immuni_common.monitoring.core import NAMESPACE, Subsystem
 
+BATCH_FILES_CREATED = Counter(
+    namespace=NAMESPACE,
+    subsystem=Subsystem.API.value,
+    name="batch_files_created",
+    documentation="Total number of created BatchFiles.",
+)
+
+BATCH_FILES_DELETED = Counter(
+    namespace=NAMESPACE,
+    subsystem=Subsystem.API.value,
+    name="batch_files_deleted",
+    documentation="Total number of deleted BatchFiles.",
+)
+
 KEYS_PROCESSED = Counter(
     namespace=NAMESPACE,
     subsystem=Subsystem.API.value,
     name="keys_processed",
-    documentation="Total number of keys processed.",
-)
-
-BATCH_FILES_CREATED = Counter(
-    namespace=NAMESPACE,
-    subsystem=Subsystem.API.value,
-    name="batches_created",
-    documentation="Total number of created batches.",
-)
-
-UPLOADS_ENQUEUED = Gauge(
-    namespace=NAMESPACE,
-    subsystem=Subsystem.API.value,
-    name="uploads_enqueued",
-    documentation="Total number of Uploads still to be processed after completing a batch",
+    documentation="Total number of processed keys.",
 )
 
 UPLOADS_DELETED = Counter(
     namespace=NAMESPACE,
     subsystem=Subsystem.API.value,
     name="uploads_deleted",
-    documentation="Total number of deleted uploads.",
+    documentation="Total number of deleted Uploads.",
 )
 
-BATCH_FILES_DELETED = Counter(
+UPLOADS_ENQUEUED = Gauge(
     namespace=NAMESPACE,
     subsystem=Subsystem.API.value,
-    name="batches_deleted",
-    documentation="Total number of deleted batches.",
+    name="uploads_enqueued",
+    documentation="Total number of Uploads still to be processed after completing a BatchFile.",
 )
