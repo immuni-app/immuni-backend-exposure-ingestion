d87527e1d72fdb29ba37fb73f3f7463cf95d280f
Gabriele Platania <gp@bendingspoons.com>
2020-06-08 12:27:13 +0200

    chore: run checks

--- immuni_exposure_ingestion/helpers/risk_level.py
+++ immuni_exposure_ingestion/helpers/risk_level.py
@@ -11,7 +11,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-from datetime import timedelta, date
+from datetime import date, timedelta
 from typing import Iterable
 
 from immuni_common.models.enums import TransmissionRiskLevel
