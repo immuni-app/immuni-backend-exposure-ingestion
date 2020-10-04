8e834681e66ca786240cf5fa4110cf146179b148
alfo1995 <damelioalfonso@hotmail.com>
2020-10-04 17:39:10 +0200

    feat: added two methods batch_it and batch_eu inside the same task to produce files both for travelers and non-travelers

--- immuni_exposure_ingestion/tasks/process_uploads.py
+++ immuni_exposure_ingestion/tasks/process_uploads.py
@@ -24,8 +24,9 @@ from immuni_common.models.mongoengine.temporary_exposure_key import TemporaryExp
 from immuni_exposure_ingestion.celery import celery_app
 from immuni_exposure_ingestion.core import config
 from immuni_exposure_ingestion.helpers.lock import lock_concurrency
-from immuni_exposure_ingestion.helpers.risk_level import extract_keys_with_risk_level_from_upload
+from immuni_exposure_ingestion.helpers.risk_level import extract_keys_with_risk_level_from_upload, extract_keys_with_risk_level_from_upload_eu
 from immuni_exposure_ingestion.models.upload import Upload
+from immuni_exposure_ingestion.models.upload_eu import UploadEu
 from immuni_exposure_ingestion.monitoring.celery import (
     BATCH_FILES_CREATED,
     KEYS_PROCESSED,
@@ -60,70 +61,145 @@ async def _process_uploads() -> None:
     # Acquire a lock on redis before processing anything, avoiding concurrent tasks.
     async with lock_concurrency("process_uploads"):
         _LOGGER.info("Obtained lock.")
-
-        infos = BatchFile.get_latest_info()
-        now = datetime.utcnow()
-
-        if infos:
-            last_period, last_index = infos
-        else:
-            last_period = datetime.fromtimestamp(
-                croniter(config.BATCH_PERIODICITY_CRONTAB).get_prev()
-            )
-            last_index = 0
-
-        period_start = last_period
-        period_end = now
-
-        _LOGGER.info(
-            "Starting to process uploads.",
-            extra=dict(period_start=period_start, period_end=period_end),
-        )
-
-        uploads = Upload.to_process()
-
-        _LOGGER.info("Uploads have been fetched.", extra=dict(n_uploads=uploads.count()))
-
-        processed_uploads: List[ObjectId] = []
-        keys: List[TemporaryExposureKey] = []
-        for upload in uploads:
-            if (reached := len(keys) + len(upload.keys)) > config.MAX_KEYS_PER_BATCH:
-                _LOGGER.warning(
-                    "Early stop: reached maximum number of keys per batch.",
-                    extra=dict(
-                        pre_reached=len(keys), reached=reached, max=config.MAX_KEYS_PER_BATCH
-                    ),
-                )
-                break
-            keys += extract_keys_with_risk_level_from_upload(upload)
-            processed_uploads.append(upload.id)
-
-        if (n_keys := len(keys)) > 0:
-            # Sort the keys. This randomizes their order (since they are random strings) so that
-            # keys of the same device are no more likely to end up consecutively.
-            keys = sorted(keys, key=lambda x: x.key_data)
-
-            index = last_index + 1
-
-            batch_file = BatchFile(
-                index=index,
-                keys=keys,
-                period_start=period_start,
-                period_end=period_end,
-                sub_batch_index=1,
-                sub_batch_count=1,
-            )
-            batch_file.client_content = batch_to_sdk_zip_file(batch_file)
-            batch_file.save()
-            _LOGGER.info("Created new batch.", extra=dict(index=index, n_keys=n_keys))
-            BATCH_FILES_CREATED.inc()
-            KEYS_PROCESSED.inc(len(keys))
-
-        Upload.set_published(processed_uploads)
-        _LOGGER.info(
-            "Flagged uploads as published.", extra=dict(n_processed_uploads=len(processed_uploads))
-        )
-        UPLOADS_ENQUEUED.set(Upload.to_process().count())
+        batch_it()
+        batch_eu()
         _LOGGER.info("Releasing lock.")
 
     _LOGGER.info("Upload processing completed successfully.")
+
+
+def batch_it():
+
+    infos = BatchFile.get_latest_info()
+    now = datetime.utcnow()
+
+    if infos:
+        last_period, last_index = infos
+    else:
+        last_period = datetime.fromtimestamp(
+            croniter(config.BATCH_PERIODICITY_CRONTAB).get_prev()
+        )
+        last_index = 0
+
+    period_start = last_period
+    period_end = now
+
+    _LOGGER.info(
+        "Starting to process uploads.",
+        extra=dict(period_start=period_start, period_end=period_end),
+    )
+
+    uploads = Upload.to_process()
+
+    _LOGGER.info("Uploads have been fetched.", extra=dict(n_uploads=uploads.count()))
+
+    processed_uploads: List[ObjectId] = []
+    keys: List[TemporaryExposureKey] = []
+    for upload in uploads:
+        if (reached := len(keys) + len(upload.keys)) > config.MAX_KEYS_PER_BATCH:
+            _LOGGER.warning(
+                "Early stop: reached maximum number of keys per batch.",
+                extra=dict(
+                    pre_reached=len(keys), reached=reached, max=config.MAX_KEYS_PER_BATCH
+                ),
+            )
+            break
+        keys += extract_keys_with_risk_level_from_upload(upload)
+        processed_uploads.append(upload.id)
+
+    if (n_keys := len(keys)) > 0:
+        # Sort the keys. This randomizes their order (since they are random strings) so that
+        # keys of the same device are no more likely to end up consecutively.
+        keys = sorted(keys, key=lambda x: x.key_data)
+
+        index = last_index + 1
+
+        batch_file = BatchFile(
+            index=index,
+            keys=keys,
+            period_start=period_start,
+            period_end=period_end,
+            sub_batch_index=1,
+            sub_batch_count=1,
+            origin="IT"
+        )
+        batch_file.client_content = batch_to_sdk_zip_file(batch_file)
+        batch_file.save()
+        _LOGGER.info("Created new batch.", extra=dict(index=index, n_keys=n_keys))
+        BATCH_FILES_CREATED.inc()
+        KEYS_PROCESSED.inc(len(keys))
+
+    Upload.set_published(processed_uploads)
+    _LOGGER.info(
+        "Flagged uploads as published.", extra=dict(n_processed_uploads=len(processed_uploads))
+    )
+    UPLOADS_ENQUEUED.set(Upload.to_process().count())
+
+
+def batch_eu():
+
+    infos = BatchFile.get_latest_info()
+    now = datetime.utcnow()
+
+    if infos:
+        last_period, last_index = infos
+    else:
+        last_period = datetime.fromtimestamp(
+            croniter(config.BATCH_PERIODICITY_CRONTAB).get_prev()
+        )
+        last_index = 0
+
+    period_start = last_period
+    period_end = now
+
+    _LOGGER.info(
+        "Starting to process uploads.",
+        extra=dict(period_start=period_start, period_end=period_end),
+    )
+
+    uploads = UploadEu.to_process()
+
+    _LOGGER.info("Uploads have been fetched.", extra=dict(n_uploads=uploads.count()))
+
+    processed_uploads: List[ObjectId] = []
+    keys: List[TemporaryExposureKey] = []
+    for upload in uploads:
+        if (reached := len(keys) + len(upload.keys)) > config.MAX_KEYS_PER_BATCH:
+            _LOGGER.warning(
+                "Early stop: reached maximum number of keys per batch.",
+                extra=dict(
+                    pre_reached=len(keys), reached=reached, max=config.MAX_KEYS_PER_BATCH
+                ),
+            )
+            break
+        keys += extract_keys_with_risk_level_from_upload_eu(upload)
+        processed_uploads.append(upload.id)
+
+    if (n_keys := len(keys)) > 0:
+        # Sort the keys. This randomizes their order (since they are random strings) so that
+        # keys of the same device are no more likely to end up consecutively.
+        keys = sorted(keys, key=lambda x: x.key_data)
+
+        index = last_index + 1
+
+        batch_file = BatchFile(
+            index=index,
+            keys=keys,
+            period_start=period_start,
+            period_end=period_end,
+            sub_batch_index=1,
+            sub_batch_count=1,
+            origin="EU",
+            batch_tag="KEYS_EU"
+        )
+        batch_file.client_content = batch_to_sdk_zip_file(batch_file)
+        batch_file.save()
+        _LOGGER.info("Created new batch.", extra=dict(index=index, n_keys=n_keys))
+        BATCH_FILES_CREATED.inc()
+        KEYS_PROCESSED.inc(len(keys))
+
+    UploadEu.set_published(processed_uploads)
+    _LOGGER.info(
+        "Flagged uploads as published.", extra=dict(n_processed_uploads=len(processed_uploads))
+    )
+    UPLOADS_ENQUEUED.set(Upload.to_process().count())
