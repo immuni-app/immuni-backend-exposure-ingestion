43f234e9ab18032733b9090be06561bf942a69eb
alfo1995 <damelioalfonso@hotmail.com>
2020-10-04 17:34:27 +0200

    feat: added method to extract keys from collection upload_eu

--- immuni_exposure_ingestion/helpers/risk_level.py
+++ immuni_exposure_ingestion/helpers/risk_level.py
@@ -19,11 +19,55 @@ from immuni_common.models.enums import TransmissionRiskLevel
 from immuni_common.models.mongoengine.temporary_exposure_key import TemporaryExposureKey
 from immuni_exposure_ingestion.core import config
 from immuni_exposure_ingestion.models.upload import Upload
+from immuni_exposure_ingestion.models.upload_eu import UploadEu
 
 _LOGGER = logging.getLogger(__name__)
 
 
 def extract_keys_with_risk_level_from_upload(upload: Upload) -> Iterable[TemporaryExposureKey]:
+    """
+    Return the keys of the given upload that are considered at risk of transmission.
+    The algorithm currently considers at maximum risk all of the keys created after two days before
+    the symptoms started.
+
+    It will also remove any keys that might still be valid.
+
+    :param upload: the upload whose keys are to be extracted from.
+    :return: the list of the given upload's keys that are considered at risk of transmission.
+    """
+
+    first_risky_time = upload.symptoms_started_on - timedelta(
+        days=config.DAYS_BEFORE_SYMPTOMS_TO_CONSIDER_KEY_AT_RISK
+    )
+
+    keys_at_risk = [key for key in upload.keys if key.created_at.date() >= first_risky_time]
+
+    for key in keys_at_risk:
+        key.transmission_risk_level = TransmissionRiskLevel.highest
+
+    # TODO: Handle current day TEKs (if any) instead of discarding them.
+    keys_at_risk_filtered = (
+        [key for key in keys_at_risk if key.expires_at < datetime.utcnow()]
+        if config.EXCLUDE_CURRENT_DAY_TEK
+        else keys_at_risk
+    )
+
+    _LOGGER.info(
+        "Extracting keys at risk from upload.",
+        extra=dict(
+            upload_id=str(upload.id),
+            symptoms_started_on=upload.symptoms_started_on,
+            first_risky_time=first_risky_time,
+            n_keys_upload=len(upload.keys),
+            n_keys_at_risk=len(keys_at_risk),
+            n_keys_at_risk_filtered=len(keys_at_risk_filtered),
+        ),
+    )
+
+    return keys_at_risk_filtered
+
+
+def extract_keys_with_risk_level_from_upload_eu(upload: UploadEu) -> Iterable[TemporaryExposureKey]:
     """
     Return the keys of the given upload that are considered at risk of transmission.
     The algorithm currently considers at maximum risk all of the keys created after two days before
