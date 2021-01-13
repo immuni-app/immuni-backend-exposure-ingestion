f1d43f097986bcae215e9bc662a4d115a38f5e92
alfo1995 <damelioalfonso@hotmail.com>
2021-01-13 20:00:09 +0100

    style: run checks

--- immuni_exposure_ingestion/helpers/his_external_service.py
+++ immuni_exposure_ingestion/helpers/his_external_service.py
@@ -52,19 +52,13 @@ def verify_cun(cun_sha: str, last_his_number: str) -> str:
     )
 
     if response.status_code == 400:
-        _LOGGER.info(
-            "Response 400 received from external service.",
-        )
+        _LOGGER.info("Response 400 received from external service.",)
         raise SchemaValidationException
     if response.status_code == 401:
-        _LOGGER.info(
-            "Response 401 received from external service.",
-        )
+        _LOGGER.info("Response 401 received from external service.",)
         raise UnauthorizedOtpException
     if response.status_code == 409:
-        _LOGGER.info(
-            "Response 409 received from external service.",
-        )
+        _LOGGER.info("Response 409 received from external service.",)
         raise OtpCollisionException
 
     try:
--- immuni_exposure_ingestion/helpers/otp_internal_service.py
+++ immuni_exposure_ingestion/helpers/otp_internal_service.py
@@ -55,14 +55,10 @@ def enable_otp(otp_sha: str, symptoms_started_on: date, id_test_verification: st
     )
 
     if response.status_code == 400:
-        _LOGGER.info(
-            "Response 400 received from external service.",
-        )
+        _LOGGER.info("Response 400 received from external service.",)
         raise SchemaValidationException
     if response.status_code == 409:
-        _LOGGER.info(
-            "Response 409 received from external service.",
-        )
+        _LOGGER.info("Response 409 received from external service.",)
         raise OtpCollisionException
 
     try:
