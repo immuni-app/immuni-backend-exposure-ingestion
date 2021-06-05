1a5dba451a7c4104a92da4cac4a6e6a48828b79d
alfo1995 <damelioalfonso@hotmail.com>
2021-06-05 12:02:56 +0200

    refactor: run poetry checks

--- immuni_exposure_ingestion/apis/ingestion.py
+++ immuni_exposure_ingestion/apis/ingestion.py
@@ -10,7 +10,6 @@
 #    GNU Affero General Public License for more details.
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program. If not, see <https://www.gnu.org/licenses/>.
-import enum
 import logging
 from datetime import date, datetime
 from http import HTTPStatus
