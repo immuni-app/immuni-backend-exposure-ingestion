7704067cc5233c05e93a38fc19867c22f8047dd4
sebaker88 <sebaker88@gmail.com>
2022-02-18 16:17:57 +0100

    fix: algorithm fixed

--- immuni_exposure_ingestion/helpers/his_external_service.py
+++ immuni_exposure_ingestion/helpers/his_external_service.py
@@ -185,8 +185,8 @@ def retrieve_dgc(
         raise ApiException
     if "fglTipoDgc" not in json_response["data"] or not json_response["data"]["fglTipoDgc"]:
         return {"qrcode": json_response["data"]["qrcode"]}
-    else:
-        return {
-            "qrcode": json_response["data"]["qrcode"],
-            "fglTipoDgc": json_response["data"]["fglTipoDgc"],
-        }
+
+    return {
+        "qrcode": json_response["data"]["qrcode"],
+        "fglTipoDgc": json_response["data"]["fglTipoDgc"],
+    }
